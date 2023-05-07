79a9488dbc8319204942aef66610ebec4e8e6cb4
Nika Kvaratskhelia <nikakvaracxeliaa@gmail.com>
2023-05-07 17:24:25 +0400

    add number on table

--- js/user.js
+++ js/user.js
@@ -8,11 +8,14 @@ fetch("https://jsonplaceholder.typicode.com/users")
       const name = document.createElement("td");
       const email = document.createElement("td");
       const phone = document.createElement("td");
+      const number = document.createElement("td")
 
+      number.textContent = item.id;
       name.textContent = item.name;
       email.textContent = item.email;
       phone.textContent = item.phone;
 
+      row.appendChild(number)
       row.appendChild(name);
       row.appendChild(email);
       row.appendChild(phone);
